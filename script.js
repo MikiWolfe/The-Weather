b9c503eff957f3d0cda78feeae0475852de196ba
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-04 16:05:40 -0800

    inital work on javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+var geoRequest ="https://openweathermap.org/api/geocoding-api"
+var weatherRequest = "https://openweathermap.org/api/geocoding-api"
+var myAPIKey = "e18c8f36cfe444e519c94f2dc3231355" // just in case 
+
+fetch(geoRequest)
+.then(function(responce)
+{return responce.json();
+})
+
+fetch();
