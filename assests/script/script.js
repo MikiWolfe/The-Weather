2749b7eae13348cb2e39259e073f81721e4bf41d
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-06 18:55:31 -0800

    pseudo coding in office hours

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,25 +1,46 @@
-var geoRequestURL ="https://openweathermap.org/api/geocoding-api";
+var geoRequestURL = "http://api.openweathermap.org/geo/1.0/direct?q=Seattle&limit=5&appid=e18c8f36cfe444e519c94f2dc3231355"
+
+// "https://openweathermap.org/api/geocoding-api";
 var weatherRequestURL = "https://openweathermap.org/api/geocoding-api";
 var myAPIKey = "e18c8f36cfe444e519c94f2dc3231355" // just in case ;
 
-var cityEl = document.getElementById("enter-city");
-var searchEl = document.getElementById("search-btn");
+var $containerEl; 
+$containerEl =$("<div class=''></div>");
+var $h2El =$("<h2 class =''>Search by City:</h2>");
+var $searchCity= $("<input class= 'col-sm-2' 'row' type='text' placeholder='Enter City:' aria-label='Enter City:'>") ;
+var $searchBtn =$("<button type='submit' class='btn btn-primary'> Search </button>");
+$containerEl.append($h2El);
+$containerEl.append($searchCity);
+$containerEl.append($searchBtn);
+$("body").append($containerEl);
 
+$("searchCity").submit(function(event){
+    event.preventDefault()
+// $(document).ready (function(){})
+// var searchHistory = localStorage.getItem('location',) 
+// localStorage.setItem('location', location)
+// form.addEventListener("submit")
+})
 
-var searchHistory = localStorage.getItem('location',) 
-
-
-localStorage.setItem('location', location)
-
-form.addEventListener("submit")
-.then()
-$(document).ready (function(){
+function geoData(cityName){
 fetch(geoRequestURL)
+
 .then(function(responce)
 {return responce.json();
 })
-
-fetch(weatherRequestURL);
-
-
+.then(function(data){
+console.log(data)
+oneCall( /*provide lat and lon here */)
 })
+
+function oneCall(lat, lon){
+    fetch(weatherRequestURL)
+    .then(function(responce){
+        return responce.json();
+    })
+    .then(function(data)
+    {console.log(data);
+    })
+}
+
+//render data to the page
