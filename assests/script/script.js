3764870d0caa845641ebb43dcbcc74442ba2b12c
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-07 14:52:24 -0800

    added function to return geolocation but using ajax. in the process of moving it back to fetch ()

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,46 +1,58 @@
-var geoRequestURL = "http://api.openweathermap.org/geo/1.0/direct?q=Seattle&limit=5&appid=e18c8f36cfe444e519c94f2dc3231355"
-
 // "https://openweathermap.org/api/geocoding-api";
-var weatherRequestURL = "https://openweathermap.org/api/geocoding-api";
-var myAPIKey = "e18c8f36cfe444e519c94f2dc3231355" // just in case ;
+var button = $("#btn");
+var weatherRequestURL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+var myAPIKey = "e18c8f36cfe444e519c94f2dc3231355"; // just in case ;
 
-var $containerEl; 
-$containerEl =$("<div class=''></div>");
-var $h2El =$("<h2 class =''>Search by City:</h2>");
-var $searchCity= $("<input class= 'col-sm-2' 'row' type='text' placeholder='Enter City:' aria-label='Enter City:'>") ;
-var $searchBtn =$("<button type='submit' class='btn btn-primary'> Search </button>");
-$containerEl.append($h2El);
-$containerEl.append($searchCity);
-$containerEl.append($searchBtn);
-$("body").append($containerEl);
-
-$("searchCity").submit(function(event){
-    event.preventDefault()
 // $(document).ready (function(){})
-// var searchHistory = localStorage.getItem('location',) 
-// localStorage.setItem('location', location)
-// form.addEventListener("submit")
-})
+button.on("click", function (e) {
+  e.preventDefault();
+  var userCity = $("#search-input").val();
+  var geoRequestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=e18c8f36cfe444e519c94f2dc3231355`;
+  //console.log("userCity", userCity);
 
-function geoData(cityName){
-fetch(geoRequestURL)
-
-.then(function(responce)
-{return responce.json();
-})
-.then(function(data){
-console.log(data)
-oneCall( /*provide lat and lon here */)
-})
-
-function oneCall(lat, lon){
-    fetch(weatherRequestURL)
-    .then(function(responce){
-        return responce.json();
+  fetch(geoRequestURL)
+    .then(function (responce) {
+      return responce.json();
     })
-    .then(function(data)
-    {console.log(data);
+    .then(function (data) {
+      console.log(data);
     })
-}
+    .then(function (geoRes) {
+      console.log(geoRes);
+      let lat = geoRes[0].lat;
+      let lon = geoRes[0].lon;
+
+      console.log("lat and lon", lat, lon);
+
+//       let oneCallApi = `https://www.google.com${lon}=${lat}`;
+
+//       $.ajax({
+//         url: oneCallApi,
+//         method: "GET",
+//       }).then(function (geoRes) {
+//         console.log(geoRes);
+//         let lat = geoRes[0].lat;
+//         let lon = geoRes[0].lon;
+
+//         console.log("lat and lon", lat, lon);
+
+//         $.ajax();
+//       });
+    });
+});
+
+// oneCall( /*provide lat and lon here */)
+// }
+// )
+// function oneCall(lat, lon){
+//     fetch(weatherRequestURL)
+//     .then(function(responce){
+//         return responce.json();
+//     })
+//     .then(function(data)
+//     {console.log(data);
+//     })
+// }
 
 //render data to the page
