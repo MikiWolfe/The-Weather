ce17aa72f5727443047365ca40f9ad119b5040ff
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-09 13:34:35 -0800

    rendering data from weather API wind in undefined

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,20 +1,33 @@
-const searchFormEl = document.getElementById("#search-form");
-let pastSearch = document.getElementById("#past-search-btns");
-const currentWeatherEl = document.getElementById("#currentWeather");
-let currentWeatherHeader = document.getElementById("#currentWeatherHeader");
-let fiveDayForecastEl = document.getElementById("#fiveDayForecast");
+const searchFormEl = document.getElementById("search-form");
+let pastSearch = document.getElementById("past-search-btns");
+const currentWeatherEl = document.getElementById("currentWeather");
+let currentWeatherHeader = document.getElementById("currentWeatherHeader");
+let fiveDayForecastEl = document.getElementById("fiveDayForecast");
+let currentTemp = document.getElementById("current-temp");
+let currentWind = document.getElementById("current-wind");
+let currentHumid = document.getElementById("current-humidity");
+let currentUV = document.getElementById("current-UV");
+let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
 
 let searchCityHistory = [];
+let userCity = null 
 const myAPIKey = "e18c8f36cfe444e519c94f2dc3231355"; // just in case ;
+
+function printResults(data) {
+  currentWeatherHeader.innerHTML ="Current City : " + userCity + " " + currentTime;
+  currentTemp.innerHTML = "Temp : " + data['current']['temp'];
+  currentWind.innerHTML = "Wind : " + data['current']['weather']['wind_speed'];
+  currentHumid.innerHTML = "Humidity : " + data['current']['humidity']
+}
 
 $("#search-form").submit(function (e) { 
 // document.forms["search-form"].submit();
-// $('#searchCityHistory').push(userCity);
   e.preventDefault();
-let userCity = $("#search-input").val();
-    // TODO: push userCity to array to store for later
-      // capitolize the city name after
-      // add past cities to list of buttons on the side. 
+ userCity = $("#search-input").val();
+ searchCityHistory.push(userCity)
+ displayPastCity()
+ 
+console.log("hi Kris" , searchCityHistory)    
    let geoRequestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=e18c8f36cfe444e519c94f2dc3231355`;
 // this function gets the lat and lon using user city data to plug in to the the weather api 
   fetch(geoRequestURL).then(async function (responce) {
@@ -31,17 +44,24 @@ function oneCall(lat, lon) {
     const data = await response.json();
 
     console.log(data);
+    printResults(data)
     return;
     
-    // TODO:this function does not work function does not come when called. 
-    function printResults(data) {
-      currentWeatherHeader.textContent = data.title;
-      currentWeatherEl.append(currentWeatherHeader);
-      console.log(userCity)
-    }
+    // TODO:this function does not work function does not come when called.  
+    
     // TODO: render() data to currentWeatherEl
     // print data to the page. would love to use dynmic HTML but understand if not today
     // 
-    printResults(); 
+   
   });
 }
+function displayPastCity(){
+  for (let i = 0; i < searchCityHistory.length; i++) {
+    let pastBtn = document.createElement("button")
+
+    pastSearch.appendChild(pastBtn)
+    // id, value, onclick and attributes
+
+  }}
+  
+  moment();
