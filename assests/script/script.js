b469b8df8bcf19d2c3e19b795cfeccc2dd0269b8
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-08 23:55:37 -0800

    styling changes to page

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,40 +1,43 @@
+const button = document.getElementById("#btn");
+const searchFormEl = document.getElementById("#search-form");
+let pastSearch = document.getElementById("#past-search-btns");
+const currentWeatherEl = document.getElementById("#currentWeather");
+let currentWeatherHeader = document.getElementById("#currentWeatherHeader");
+let fiveDayForecastEl = document.getElementById("#fiveDayForecast");
 
-const button =document.getElementById("#btn")
-const searchFormEl = document.getElementById("#search-form")
+let searchCityHistory = [];
 const myAPIKey = "e18c8f36cfe444e519c94f2dc3231355"; // just in case ;
 
-
-$("#search-form").submit( function (e) {
+$("#search-form").submit(function (e) { 
+// document.forms["search-form"].submit();
   e.preventDefault();
-  let userCity = $("#search-input").val();
+let userCity = $("#search-input").val();
+    $('#searchCityHistory').push(userCity);
+ 
+
   let geoRequestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=e18c8f36cfe444e519c94f2dc3231355`;
-  
-  fetch(geoRequestURL)
-  .then(async function (responce) {
-    const data= await responce.json();
-    
-    console.log(data); 
+
+  fetch(geoRequestURL).then(async function (responce) {
+    const data = await responce.json();
     const lat = data[0].lat;
     const lon = data[0].lon;
-    
-    console.log("lat and lon", lat, lon);
-    oneCall(lat, lon)    
-  })
-  
+    oneCall(lat, lon);
+  });
 });
+function oneCall(lat, lon) {
+  var weatherRequestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely,hourly,daily&units=imperial&appid=e18c8f36cfe444e519c94f2dc3231355`;
+  fetch(weatherRequestURL).then(async function (response) {
+    const data = await response.json();
 
-function oneCall(lat, lon){
-      var weatherRequestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely,hourly,daily&units=imperial&appid=e18c8f36cfe444e519c94f2dc3231355`;
-      fetch(weatherRequestURL)
-        .then(async function(response){
-        const data = await response.json();  
-          
-        console.log(data)
-// let htlm =data.data.map(current =>{
-  // return 'current' + current.temp
-// })
-// render(data){}'The Weather today for' + 'userCity' ':'
-
-
-
-})}  
+    console.log(data);
+    return;
+    
+    function printResults(data) {
+      currentWeatherHeader.textContent = data.title;
+      currentWeatherEl.append(currentWeatherHeader);
+      console.log(userCity)
+    }
+    // render() data to currentWeatherEl
+    printResults();
+  });
+}
