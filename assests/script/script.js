02b288bd762a4c3a0c5a25f03ed0fd2e9c29b3c3
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-10 00:07:27 -0800

    attempting to make local storage work. Also attmepted to get cards to work.

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,4 +1,4 @@
-const searchBtn = document.getElementById("search-btn")
+const searchBtn = document.getElementById("search-btn");
 const searchFormEl = document.getElementById("search-form");
 let pastSearch = document.getElementById("past-search-btns");
 const currentWeatherEl = document.getElementById("currentWeather");
@@ -8,36 +8,64 @@ let currentTemp = document.getElementById("current-temp");
 let currentWind = document.getElementById("current-wind");
 let currentHumid = document.getElementById("current-humidity");
 let currentUV = document.getElementById("current-UV");
-let currentTime = moment().format("MMMM Do YYYY");
-// let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+let dayOne = document.getElementById("dayOne");
+let currentDay = moment().format("MMMM Do YYYY");
 
 let searchCityHistory = [];
 let userCity = null;
 const myAPIKey = "e18c8f36cfe444e519c94f2dc3231355"; // just in case ;
+
 moment();
+
 function printResults(data) {
+  let weatherIcon = data.current.weather[0].icon;
+  let iconURL = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+  img = document.createElement('img')
+  img.src = iconURL
   currentWeatherHeader.innerHTML =
-    "Current City : " +
-    userCity +
-    " " +
-    currentTime +
-    data.current.weather[0].icon;
-    // Coverting celsius to fahrenheit
-    let celsius = data["current"]["temp"]
-    var fahr = ((celsius * 9) /5 ) + 32;
-  currentTemp.innerHTML = "Temp : " + fahr + "°F"  + " / " + celsius  + "°C";
+    "Current City : " + userCity + " : " + currentDay;
+  currentWeatherHeader.appendChild(img)
+  currentTemp.innerHTML = "Temp : " + data["current"]["temp"] +"°F" ;
   currentWind.innerHTML = "Wind : " + data["current"]["wind_speed"] + " MPH";
   currentHumid.innerHTML = "Humidity : " + data["current"]["humidity"] + "%";
   currentUV.innerHTML = "UV Index : " + data.current.uvi;
 }
+function displayFiveDay(data) {
+  let date = currentDay;
+  document.getElementById('dayOne').innerHTML = date
+  let dailyIcon = data.daily[0].weather[0].icon
+  let iconURL = `https://openweathermap.org/img/wn/${dailyIcon}@2x.png`;
+  img = document.createElement('img')
+  img.src = iconURL
+  document.getElementById("cardOne").appendChild(img)
+  // for (let i = 0; i < 5; i++) {
+  
+  //   //  let temp =
+  //   let celsius = data["current"]["temp"];
+  //   let fahr = (celsius * 9) / 5 + 32;
+  //   let wind = data.daily[i + 1].wind_speed;
+  //   let humid = data.daily[i + 1].humidity;
 
+  //   dayOne.innerHTML = data.daily[1].temp.day;
+  // }
+
+  // div class="card">
+  // <div class="card-body">
+  //   <h5 class="card-title">Date:</h5>
+  //   <p class="card-text">Temp:</p>
+  //   <p class="card-text">Wind:</p>
+  //   <p class="card-text">Humidity:</p>
+
+  // </div>
+}
 $("#search-form").submit(function (e) {
   e.preventDefault();
   userCity = $("#search-input").val();
   searchCityHistory.push(userCity);
+  localStorage.setItem("userCity", JSON.stringify(searchCityHistory));
   displayPastCity();
-
-  let geoRequestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=${myAPIKey}`;
+  
+  let geoRequestURL = `https://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=${myAPIKey}`;
   // this function gets the lat and lon using user city data to plug in to the the weather api
   fetch(geoRequestURL).then(async function (responce) {
     const data = await responce.json();
@@ -48,35 +76,39 @@ $("#search-form").submit(function (e) {
 });
 // this function gets weather data from API
 function oneCall(lat, lon) {
-  var weatherRequestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely,hourly,daily&units=imperial&appid=${myAPIKey}`;
+  var weatherRequestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely,hourly,&units=imperial&appid=${myAPIKey}`;
   fetch(weatherRequestURL).then(async function (response) {
     const data = await response.json();
-
+    
     console.log(data);
     printResults(data);
+    displayFiveDay()
+
     return;
   });
 }
+
 function displayPastCity() {
   for (let i = 0; i < searchCityHistory.length; i++) {
-    let pastBtn = document.createElement("button");
-
+    let pastBtn = document.createElement("button");     
+    pastBtn.classList.add("btn", "btn-secondary", "btn-block", "caps");
+    // pastBtn.setAttribute(
+    //   "onClick",
+    //   "(this.value)"
+    // );
+    pastBtn.addEventListener("click", function(){
+    pastBtn.value  
     pastSearch.appendChild(pastBtn);
-    pastBtn.classList.add('btn', 'btn-secondary', 'btn-block')
-
-    // id, value, onclick and attributes
-    //  <button class="btn btn-secondary btn-block">Past City</button>
+    })
+    
   }
-};
+}
 
 // clear local storage
-clearBtn = document.createElement("button")
-clearBtn.classList.add('btn-danger', 'btn-block' , 'btn')
+clearBtn = document.createElement("button");
+clearBtn.classList.add("btn-danger", "btn-block", "btn", "caps");
 clearBtn.innerHTML = "Clear Search";
-pastSearch.appendChild(clearBtn)
-pastSearch.addEventListener("click", function(){
-  localStorage.clear()
-})
-
-
-
+pastSearch.appendChild(clearBtn);
+pastSearch.addEventListener("click", function () {
+  localStorage.clear();
+});
