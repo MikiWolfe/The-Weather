8c9331a4575a5d8637c56dddc35dc5df3167da6d
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-13 12:17:21 -0800

    cleared out previous five day forecast and also minor styling changes to cards.

--- assests/script/script.js
+++ assests/script/script.js
@@ -3,12 +3,11 @@ const searchFormEl = document.getElementById("search-form");
 let pastSearch = document.getElementById("past-search-btns");
 const currentWeatherEl = document.getElementById("currentWeather");
 let currentWeatherHeader = document.getElementById("currentWeatherHeader");
-let fiveDayForecastEl = document.getElementById("fiveDayForecast");
 let currentTemp = document.getElementById("current-temp");
 let currentWind = document.getElementById("current-wind");
 let currentHumid = document.getElementById("current-humidity");
 let currentUV = document.getElementById("current-UV");
-let dateOne = document.getElementById("headerOne");
+let fiveDayForecastEl = document.getElementById("fiveDayForecast");
 let currentDay = moment().format("MMMM Do YYYY");
 let searchCityHistory = [];
 let userCity = null;
@@ -20,8 +19,7 @@ function printResults(data) {
   let iconURL = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png `;
   img = document.createElement("img");
   img.src = iconURL;
-  currentWeatherHeader.innerHTML =
-    "Current City : " + userCity + " : " + currentDay;
+  currentWeatherHeader.innerHTML = userCity + " : " + currentDay;
   currentWeatherHeader.append(img);
   currentTemp.innerHTML = "Temp : " + data["current"]["temp"] + "°F";
   currentWind.innerHTML = "Wind : " + data["current"]["wind_speed"] + " MPH";
@@ -37,26 +35,25 @@ function printResults(data) {
 }
 
 function displayFiveDay(data) {
-  console.log(data);
+fiveDayForecastEl.innerHTML = " "
+ console.log(data);
   for (let i = 1; i < 6; i++) {
     let fiveCard = $("<div>").attr(
-      "class",
-      "bg-secondary text-white",
-      "col-md-1 border border-primary"
-    );
-    $("#fiveDayForecast").append(fiveCard);
-    let time = moment().add(i, "day").format("MMM Do YY");
-
-    let day = $("<p>").html(time);
-    fiveCard.append(day);
-    let fiveIcon = data.daily[i].weather[0].icon;
-    let iconURL = `https://openweathermap.org/img/wn/${fiveIcon}@2x.png`;
-    img = document.createElement("img");
-    img.src = iconURL;
-    fiveCard.append(img);
-    let fiveTemp = "Temp:" + data.daily[i].temp.day + "°F";
-    let temp = $("<p>").html(fiveTemp);
-    fiveCard.append(temp);
+      "class" , " card - body bg-secondary text-white col-sm-5"
+      );
+      $("#fiveDayForecast").append(fiveCard);
+      let time = moment().add(i, "day").format("MMM Do YY");
+      
+      let day = $("<p>").html(time);
+      fiveCard.append(day);
+      let fiveIcon = data.daily[i].weather[0].icon;
+      let iconURL = `https://openweathermap.org/img/wn/${fiveIcon}@2x.png`;
+      img = document.createElement("img");
+      img.src = iconURL;
+      fiveCard.append(img);
+      let fiveTemp = "Temp:" + data.daily[i].temp.day + "°F";
+      let temp = $("<p>").html(fiveTemp);
+      fiveCard.append(temp);
     let fiveWind = "Wind spd:" + "  " + data.daily[i].wind_speed + " " + "MPH";
     let wind = $("<p>").html(fiveWind);
     fiveCard.append(wind);
@@ -73,7 +70,7 @@ $("#search-form").submit((e) => {
   searchCityHistory.push(userCity);
   localStorage.setItem("userCity", JSON.stringify(searchCityHistory));
   // displayPastCity();
-
+  
   let geoRequestURL = `https://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=${myAPIKey}`;
   // this function gets the lat and lon using user city data to plug in to the the weather api
   fetch(geoRequestURL).then(async function (responce) {
@@ -88,34 +85,33 @@ function oneCall(lat, lon) {
   var weatherRequestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely,hourly,&units=imperial&appid=${myAPIKey}`;
   fetch(weatherRequestURL).then(async function (response) {
     const data = await response.json();
-    printResults(data);
-    // fiveDayForecastEl.textContent("")
-
+    printResults(data);    
     displayFiveDay(data);
-    return;
+    return; 
   });
 }
 
 // TODO:
-// function displayPastCity() {
-//    {
-//     let cityHistory = JSON.parse(localStorage.getItem("searchCityHistory"));
-//     if(!cityHistory == null){
-//       searchCityHistory = cityHistory
-//     };
+function displayPastCity() {
+   {
+    let cityHistory = JSON.parse(localStorage.getItem("searchCityHistory"));
+    if(!cityHistory == null){
+      searchCityHistory = cityHistory
+    };
 
-//     for (let i = 0; i < searchCityHistory.length; i++){
-//       if (i === 8){
-//         break;
-//       }
-//     let pastBtn = document.createElement("button");
-//     pastBtn.classList.add("btn", "btn-secondary", "btn-block", "caps");
-//     pastBtn.setAttribute("value", searchCityHistory[i]);}
-//     pastBtn.text(searchCityHistory[i]);
-//     pastBtn.addEventListener("click", function () {});
-//     pastSearch.appendChild(pastBtn);
-//   }
-// }
+    for (let i = 0; i < searchCityHistory.length; i++){
+      if (i === 8){
+        break;
+      }
+    let pastBtn = document.createElement("button");
+    pastBtn.classList.add("btn", "btn-secondary", "btn-block", "caps");
+    pastBtn.setAttribute("value", searchCityHistory[i]);}
+    pastBtn.text(searchCityHistory[i]);
+    pastBtn.addEventListener("click", function () {});
+    pastSearch.appendChild(pastBtn);
+  }
+}
+
 // clear local storage
 clearBtn = document.createElement("button");
 clearBtn.classList.add("btn-danger", "btn-block", "btn", "caps");
